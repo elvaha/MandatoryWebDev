f3bf5da7b00511f2828e95063071f61f60ad5a0f
elvaha <elias.v.hansen@gmail.com>
2016-10-10 12:24:53 +0200

    real commit

---
+++ MandatoryWebDev/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,32 @@
+﻿using MandatoryWebDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace MandatoryWebDev.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormModelView());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactFormModelView model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            else
+            {
+                return RedirectToCurrentUmbracoPage();
+            }
+        }
+    }
+}
---
+++ MandatoryWebDev/Controllers/MessageFormSurfaceController.cs
@@ -0,0 +1,25 @@
+﻿using MandatoryWebDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace MandatoryWebDev.Controllers
+{
+    public class MessageFormSurfaceController : SurfaceController
+    {
+        // GET: MessageFormSurface
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostMessage(MessageFormModelView model)
+        {
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
