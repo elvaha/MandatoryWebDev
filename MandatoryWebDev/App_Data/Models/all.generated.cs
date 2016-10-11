06a7a7f42832a0d36c5864aec6ec1b340d019cbe
elvaha <elias.v.hansen@gmail.com>
2016-10-11 13:31:17 +0200

    last doc types

--- MandatoryWebDev/App_Data/Models/all.generated.cs
+++ MandatoryWebDev/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "278c240ee56a1c0c")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
